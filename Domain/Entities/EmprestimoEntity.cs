e27c6f927ea1e93e84a42a0666bc66fa6d520f35
maxpayne301 <pretteviski@hotmail.com>
2026-03-18 13:14:11 -0300

    feat: only generate fine when return is past due date

--- Domain/Entities/EmprestimoEntity.cs
+++ Domain/Entities/EmprestimoEntity.cs
@@ -44,6 +44,10 @@ public class EmprestimoEntity
             throw new Exception("Empréstimo ainda não devolvido.");
         
         TimeSpan atraso = DataDevolucao.Value - DataPrevistaDevolucao;
+
+        if (atraso.Days <= 0)
+            return 0;
+
         decimal valorMulta = (decimal)atraso.Days * 2.00m; // Exemplo: R$2,00 por dia de atraso
         return valorMulta;
         
